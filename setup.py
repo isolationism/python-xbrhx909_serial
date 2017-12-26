c9bc88ec2354245f6e51684085ee25bcd5d10b1d
Kevin Williams <kevin@isolationism.com>
2017-12-25 21:03:09 -0500

    refactor(): Shuffle some stuff around, Python 3 compatibility

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 
-# Distutils is the default python packager utility.
-#from distutils.core import setup
-
 # Setuptools is a slightly nicer distribution utility that can create 'eggs'.
 from setuptools import setup, find_packages
 
 setup(name='xbrhx909_serial',
-    version='0.0.1',
+    version='0.9.0',
     description='Serial Control Protocol for Sony Television model XBR HX909 (and similar devices)',
     author='Kevin Williams',
     author_email='kevin@weblivion.com',
